a8b3b04bae15037bf465c8422f102932312cfe63
Debora Maxinas <md.dumuta@gmail.com>
2024-01-31 15:03:22 +0200

    second GUI

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,32 @@ public class Main {
 
         public static void main(String[] args) {
             JFrame frame = new JFrame("Calculator");
-            frame.setSize(500, 400);
+            frame.setSize(300, 500);
             frame.setLocation(200, 200);
             LayoutManager manager = new FlowLayout();
             frame.setLayout(manager);
-            frame.getContentPane().add(new TextField("    1"));
-            frame.getContentPane().add(new JLabel("+"));
-            frame.getContentPane().add(new TextField("    2"));
-            frame.getContentPane().add(new JLabel("="));
-            frame.getContentPane().add(new TextField("    3"));
+            frame.getContentPane().add(new JButton("1"));
+            frame.getContentPane().add(new JButton("2"));
+            frame.getContentPane().add(new JButton("3"));
+            frame.getContentPane().add(new JButton("4"));
+            frame.getContentPane().add(new JButton("5"));
+            frame.getContentPane().add(new JButton("6"));
+            frame.getContentPane().add(new JButton("7"));
+            frame.getContentPane().add(new JButton("8"));
+            frame.getContentPane().add(new JButton("9"));
+            frame.getContentPane().add(new JButton("0"));
+            frame.getContentPane().add(new JButton("+"));
+            frame.getContentPane().add(new JButton("-"));
+            frame.getContentPane().add(new JButton("/"));
+            frame.getContentPane().add(new JButton("*"));
+            frame.getContentPane().add(new JButton("="));
+            frame.getContentPane().add(new JButton("C"));
+
+
+        //    frame.getContentPane().add(new JLabel("*"));
+//            frame.getContentPane().add(new TextField("    2"));
+//            frame.getContentPane().add(new Container());
+//            frame.getContentPane().add(new TextField("    3"));
 //            int a = 1;
 //            String A = Integer.toString(a);
 //            int b = 2;
